11b767d476b0e7417d79eaf0984b4a8f72441630
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-02-22 11:37:49 +0530

    Set Error

--- src/Utils/Validate.js
+++ src/Utils/Validate.js
@@ -1,7 +1,7 @@
 export const Validate = (email,password)=>{
     const Email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
     const Password = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
-    const Name = /^[A-Za-z]+(?:[ '-][A-Za-z]+)*$/.test(name); 
+    // const Name = /^[A-Za-z]+(?:[ '-][A-Za-z]+)*$/.test(name); 
     if(!Email){
         return "Email is not Valid"
     }
