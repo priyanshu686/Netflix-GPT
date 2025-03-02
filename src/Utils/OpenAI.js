6469ba5a7586ac58a5bec59064f6739e712fdf26
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-03-02 13:48:05 +0530

    Change in the key

--- src/Utils/OpenAI.js
+++ src/Utils/OpenAI.js
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
-const OPEN_AI_KEY = process.env.OPEN_AI_KEY;
+const OPEN_AI_KEY = process.env.REACT_APP_OPEN_AI_KEY;
 
 const openaicall = new OpenAI({
   apiKey: OPEN_AI_KEY, // This is the default and can be omitted
