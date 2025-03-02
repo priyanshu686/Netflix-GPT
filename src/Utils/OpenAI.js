ce9210af040261d754e02f8758d7b8e00ce813a8
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-03-02 13:48:30 +0530

    Remove extra stuff

--- src/Utils/OpenAI.js
+++ src/Utils/OpenAI.js
@@ -3,7 +3,7 @@ import OpenAI from 'openai';
 const OPEN_AI_KEY = process.env.REACT_APP_OPEN_AI_KEY;
 
 const openaicall = new OpenAI({
-  apiKey: OPEN_AI_KEY, // This is the default and can be omitted
+  apiKey: OPEN_AI_KEY, 
   dangerouslyAllowBrowser: true
 
 });
